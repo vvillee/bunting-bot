823582be880a09a4656a9a6c70e2b44d661d1675
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2016-02-02 00:06:52 +0200

    initial commit

---
+++ bot.js
@@ -0,0 +1,17 @@
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: 'xoxb-20033792914-3LxaaHa2D8pKLtuM3dQ8OgfN'
+})
+bot.startRTM(function(err,bot,payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
+});
+controller.hears(["keyword","^pattern$"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+  // do something to respond to message
+  // all of the fields available in a normal Slack message object are available
+  // https://api.slack.com/events/message
+  bot.reply(message,'You used a keyword!');
+});
+
