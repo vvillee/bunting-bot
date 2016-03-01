6c632d5311b714019616a70aef958d4e2c10b889
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2016-03-02 00:51:38 +0200

    initial working draft

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
+if (!process.env.SLACK_TOKEN) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
 var Botkit = require('botkit');
+var http = require('http');
 var controller = Botkit.slackbot({
   debug: false
-  //include "log: false" to disable logging
-  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
 // connect the bot to a stream of messages
@@ -11,13 +15,47 @@ controller.spawn({
 }).startRTM()
 
 // give the bot something to listen for.
-controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
-  bot.reply(message,'Hello yourself.');
-});
+controller.hears(
+  'keltasirkku',
+  ['direct_message','direct_mention','mention','ambient'],
+  function (bot, message) {
+    var reply = function (replyMessage) {
+      bot.reply(message, replyMessage);
+    };
+    var fetchMenu = function () {
+      var url = 'http://ruokalistat.leijonacatering.fi/AromiStorage/blob/menu/4e605ac2-acdb-e511-8349-78e3b50298fc';
+
+      http.get(url, function (res) {
+        var body = '';
+        
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        
+        res.on('end', function () {
+          var response = JSON.parse(body);
+          var today = new Date();
+          var replyMessage = '';
+          try {
+            response.Days[today.getDay() - 1].Meals.forEach(function(meal){
+              console.log(meal.Name);
+              replyMessage = replyMessage + meal.MealType + ': ' + meal.Name + '\n';
+            });
+          } catch (e) {
+            replyMessage = 'Ei ruokaa tälle päivälle :(';
+          }
+          reply(replyMessage);
+        });
+      
+      }).on('error', function (e) { console.log("Got an error: ", e) });
+    };
+    fetchMenu();
+  }
+);
 
 // to avoid heroku error, listen port process.env.PORT
 var http = require('http');
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.send('it is running\n');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.send('it is running\n');
 }).listen(process.env.PORT || 5000);
