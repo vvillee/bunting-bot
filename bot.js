e040f706d222d4ccabc55d0ca993d92646e43724
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2016-03-01 23:32:19 +0200

    add some intendation

--- bot.js
+++ bot.js
@@ -15,4 +15,9 @@ controller.hears('hello',['direct_message','direct_mention','mention'],function(
   bot.reply(message,'Hello yourself.');
 });
 
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+// to avoid heroku error, listen port process.env.PORT
+var http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.send('it is running\n');
+}).listen(process.env.PORT || 5000);
