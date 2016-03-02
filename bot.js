48e26a228fd56d261f131c65ee250196c157b3a9
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2016-03-02 18:31:27 +0200

    fix typo

--- bot.js
+++ bot.js
@@ -65,5 +65,5 @@ controller.hears(
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.send('it is running\n');
+  res.end('it is running\n');
 }).listen(process.env.PORT || 5000);
