3887fb7ef5fb2d583781a9fe1cf29292d0a94d4a
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2016-11-09 23:48:31 +0200

    Revert "No need for this anymore"
    This reverts commit 5f05f72e695c832d1764bb9a05e4f9612dad1380.

--- bot.js
+++ bot.js
@@ -140,3 +140,9 @@ controller.hears(
     handleChannelData(message.channel);
   }
 );
+
+// to avoid heroku error, listen port process.env.PORT
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('it is running\n');
+}).listen(process.env.PORT || 5000);
